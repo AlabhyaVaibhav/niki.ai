ba22f424f0f1090d7d63421aee6b90ccf60b8685
Alabhya <alabhya.vaibhav1997@gmail.com>
2017-04-04 12:19:30 +0530

    Update a3.py
    bugs fixed

--- a3.py
+++ a3.py
@@ -2,18 +2,21 @@ def cipher(m,n):
 	alpha_list=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 	if m<n:
 		if m<=26 and n <=26:
-			for x in range(m,n):
+			for x in range(m-1,n):
 				print(alpha_list[x])
 		elif m <=26 and n >=26:
+			print ("Case 2")
 			while m<=26:
-				print(alpha_list[m])
+				print (alpha_list[m-1])
 				m=m+1
-			for x in range(m,n):
-				fa=x/26
-				sa=x-(26*fa)
-				print(alpha_list[fa],alpha_list[sa])
+			m=m-1
+			for x in range(m,n+1):
+			  fa=int(x/26)
+			  sa=int(x-(26*fa))
+			  print(alpha_list[int(fa-1)] + alpha_list[int(sa)])
 		elif m>26 and n>26:
-			for x in range(m,n):
-				fa=x/26
-				sa=x-(26*fa)
-				print(alpha_list[fa],alpha_list[sa])
+			for x in range(m-1,n):
+				fa=int(x/26)
+				sa=int(x-(26*fa))
+				print(alpha_list[int(fa-1)] + alpha_list[int(sa)])
+
