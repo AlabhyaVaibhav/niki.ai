0d96e967588235858ae7d59473ac421def5ea2b5
Alabhya <alabhya.vaibhav1997@gmail.com>
2017-04-04 09:39:26 +0530

    Delete a1.py

--- a1.py
+++
@@ -1,7 +0,0 @@
-def StringSort(stringList):
-    print (sorted(stringList, key=len))
-no = input("Number of strings to sort :")
-stringList = []
-for i in range(int(no)):
-	stringList.append(input("Enter string"))	
-StringSort(stringList)
